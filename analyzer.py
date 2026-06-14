75dddcb9da7195665cb42ac17053fa4736dca0d6
NADAVAPALLI-SAI-PRANAV-BHASKAR <148541471+NADAVAPALLI-SAI-PRANAV-BHASKAR@users.noreply.github.com>
2026-06-15 05:29:20 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -31,3 +31,24 @@ print("\nPotential Brute Force Sources:")
 for ip, count in failed_count.items():
     if count >= 5:
         print(ip)
+
+with open("report.txt", "w") as report:
+    report.write("===== LogSleuth Report =====\n\n")
+
+    report.write("Top IP Addresses:\n")
+    for ip, count in ip_count.most_common():
+        report.write(f"{ip} - {count} requests\n")
+
+    report.write("\nPotential Brute Force Sources:\n")
+
+    found = False
+
+    for ip, count in failed_count.items():
+        if count >= 5:
+            report.write(f"{ip}\n")
+            found = True
+
+    if not found:
+        report.write("None\n")
+
+print("\nReport saved as report.txt")
